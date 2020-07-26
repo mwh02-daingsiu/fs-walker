1a4538882fca41e3605756f40789aef9ee7778c6
mwh02 <mwh02.daingsiu@gmail.com>
2020-07-27 00:46:19 +0800

    Now it works in fs blocks unit

--- gendust.c
+++ gendust.c
@@ -10,32 +10,38 @@
 #include <inttypes.h>
 #include <sys/random.h>
 
+static const unsigned int block_size = 4096;
+
 static void
-gen_bad_blocks(uint64_t device_size, uint64_t nr_bblock, uint64_t start, FILE *fout)
+gen_bad_blocks(uint64_t fs_blocks, uint64_t nr_bblock, uint64_t start_block, FILE *fout)
 {
 	uint64_t i;
-	uint64_t offset = start;
+	uint64_t randu64;
+	uint64_t offset = start_block;
+
+	if (getrandom(&randu64, sizeof(randu64), 0) == -1)
+		fprintf(stderr, "Warning: the %" PRIu64 "th block may not be random\n", i);
+
+	srandom(randu64);
 
 	for (i = 0; i < nr_bblock; i++) {
-		uint64_t randu64;
 		uint64_t block = offset;
-		if (getrandom(&randu64, sizeof(randu64), 0) == -1)
-			fprintf(stderr, "Warning: the %" PRIu64 "th block may not be random\n", i);
-		block += randu64 % (device_size - offset);
+		block += random() % (fs_blocks - offset);
 		if (fout)
 			fprintf(fout, "%" PRIu64 "\n", block);
 	}
 }
 
 //
-// Usage: gendust device_size number_of_bad_blocks start_offset
+// Usage: gendust device_size number_of_bad_blocks start_block
 //
 int
 main(int argc, char **argv)
 {
 	unsigned long long device_size = 0;
 	unsigned long long nr_bblocks = 0;
-	unsigned long long start = 0;
+	unsigned long long start_block = 0;
+	unsigned long long fs_blocks = 0;
 
 	if (argc != 4)
 		return EXIT_FAILURE;
@@ -51,12 +57,14 @@ main(int argc, char **argv)
 		fprintf(stderr, "Failed to parse number_of_bad_blocks\n");
 		return EXIT_FAILURE;
 	}
-	start = strtoull(argv[3], NULL, 0);
+	start_block = strtoull(argv[3], NULL, 0);
 	if (errno) {
-		fprintf(stderr, "Failed to parse start_offset\n");
+		fprintf(stderr, "Failed to parse start_block\n");
 		return EXIT_FAILURE;
 	}
 
-	gen_bad_blocks(device_size, nr_bblocks, start, stdout);
+	fs_blocks = device_size / (block_size / 512);
+
+	gen_bad_blocks(fs_blocks, nr_bblocks, start_block, stdout);
 	return EXIT_SUCCESS;
 }
