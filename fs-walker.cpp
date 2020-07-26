5be4ef29d5139c4a57644d1bd6e2965dbbe809e9
mwh02 <mwh02.daingsiu@gmail.com>
2020-07-26 19:13:12 +0800

    Add big file checking logic

--- fs-walker.cpp
+++ fs-walker.cpp
@@ -19,7 +19,16 @@ static unsigned long long status_partial_directory = 0;
 static unsigned long long status_partial_file = 0;
 static unsigned long long status_undefined_error = 0;
 
-static const unsigned long max_legal_readunit = 65536;
+static unsigned long long status_all_file_total_chunks = 0;
+static unsigned long long status_all_file_missing_chunks = 0;
+static unsigned long long status_all_file_good_chunks = 0;
+
+static unsigned long long status_huge_file_total_chunks = 0;
+static unsigned long long status_huge_file_missing_chunks = 0;
+static unsigned long long status_huge_file_good_chunks = 0;
+
+static unsigned long max_legal_readunit = 65536;
+static off_t huge_file_thresh = 16384ull * 1024 * 1024 * 1024;
 
 static void
 visit_file(const char *filename, struct stat *statbuf)
@@ -27,8 +36,10 @@ visit_file(const char *filename, struct stat *statbuf)
 	off_t filesize = statbuf->st_size;
 	unsigned long readunit = statbuf->st_blksize;
 	std::vector<char> buf(readunit);
-	off_t rem = filesize;
+	off_t off = 0, readbytes = 0;
 	int fd = -1;
+	int is_huge_file = !!(filesize >= huge_file_thresh);
+	int has_error = 0;
 
 	if (readunit == 0 || readunit > max_legal_readunit) {
 		goto error_file;
@@ -39,16 +50,36 @@ visit_file(const char *filename, struct stat *statbuf)
 		goto error_file;
 	}
 
-	while (rem > 0) {
-		ssize_t r = read(fd, &buf[0], (rem < readunit) ? rem : readunit);
+	while (off < filesize) {
+		size_t xmit = (filesize - off < readunit) ? filesize - off : readunit;
+		ssize_t r = pread(fd, &buf[0], xmit, off);
+		if (r != 0) {
+			status_all_file_total_chunks++;
+			if (is_huge_file)
+				status_huge_file_total_chunks++;
+		}
 		if (r < 0) {
-			if (rem == filesize)
-				goto error_file;
-			goto partial_file;
+			status_all_file_missing_chunks++;
+			if (is_huge_file)
+				status_huge_file_missing_chunks++;
+
+			has_error = 1;
+		} else if (r == 0) {
+			break;
+		} else {
+			status_all_file_good_chunks++;
+			if (is_huge_file)
+				status_huge_file_good_chunks++;
+
 		}
 
-		rem -= r;
+		if (r > 0)
+			readbytes += r;
+		off += xmit;
 	}
+
+	if (has_error)
+		goto error_file;
 
 ok_file:
 	status_completed_file++;
@@ -56,13 +87,10 @@ ok_file:
 	return;
 
 error_file:
-	status_error_file++;
-	if (fd != -1)
-		close(fd);
-	return;
-
-partial_file:
-	status_partial_file++;
+	if (!readbytes)
+		status_error_file++;
+	else
+		status_partial_file++;
 	if (fd != -1)
 		close(fd);
 }
@@ -102,6 +130,14 @@ print_stat()
 	printf("status_partial_directory: %llu\n", status_partial_directory);
 	printf("status_partial_file: %llu\n", status_partial_file);
 	printf("status_undefined_error: %llu\n", status_undefined_error);
+	printf("=================================\n");
+	printf("status_all_file_total_chunks: %llu\n", status_all_file_total_chunks);
+	printf("status_all_file_missing_chunks: %llu\n", status_all_file_missing_chunks);
+	printf("status_all_file_total_chunks: %llu\n", status_all_file_good_chunks);
+	printf("================================= (For files bigger or equal to %lld)\n", (long long)huge_file_thresh);
+	printf("status_huge_file_total_chunks: %llu\n", status_huge_file_total_chunks);
+	printf("status_huge_file_missing_chunks: %llu\n", status_huge_file_missing_chunks);
+	printf("status_huge_file_total_chunks: %llu\n", status_huge_file_good_chunks);
 }
 
 int
@@ -109,9 +145,18 @@ main(int argc, char **argv)
 {
 	FTS *fts;
 	FTSENT *ent;
+	char *v;
 
 	if (argc < 2)
 		return EXIT_FAILURE;
+
+	v = getenv("huge_file_thresh");
+	if (v) {
+		errno = 0;
+		unsigned long long vi = strtoull(v, NULL, 0);
+		if (!errno)
+			huge_file_thresh = vi;
+	}
 
 	fts = fts_open(argv + 1, FTS_PHYSICAL | FTS_XDEV, NULL);
 
