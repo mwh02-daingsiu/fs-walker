48911464c905688bc0b88c75a7bcb6977b381a16
mwh02 <mwh02.daingsiu@gmail.com>
2020-07-27 00:03:19 +0800

    Pump

---
+++ ADD_BBLOCKS.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+INPUT_BBLIST=$1
+
+if [[ -z $INPUT_BBLIST ]]; then
+	echo "No input badblock list specified!" > /dev/stderr
+	exit 1
+fi
+
+for bblock in `cat "$INPUT_BBLIST"`; do
+	dmsetup message $DM_NAME 0 addbadblock $((bblock / 8))
+done
--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
+TARGETS=fs-walker gendust
+
+all: $(TARGETS)
+
 fs-walker: fs-walker.c
 	$(CC) -ggdb $^ -o $@
+
+gendust: gendust.c
+	$(CC) -ldevmapper -ggdb $^ -o $@
+
+clean:
+	rm -f $(TARGETS)
